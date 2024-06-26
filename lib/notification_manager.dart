76a3cdba7d720f5f611943d1b789446000451fd7
DontitoSama <jokonito06@gmail.com>
2024-06-26 10:36:23 +0200

    :pencil: Refresh

--- lib/notification_manager.dart
+++ lib/notification_manager.dart
@@ -5,19 +5,22 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 class NotificationManager {
   static FlutterLocalNotificationsPlugin? _flutterLocalNotificationsPlugin;
 
+  // Liste des citations de Star Wars
   static List<String> starWarsQuotes = [
-    "May the Force be with you. - Obi-Wan Kenobi",
-    "The Force will be with you, always. - Obi-Wan Kenobi",
-    "I find your lack of faith disturbing. - Darth Vader",
-    "Do. Or do not. There is no try. - Yoda",
-    "In my experience, there's no such thing as luck. - Obi-Wan Kenobi",
-    "Fear is the path to the dark side. Fear leads to anger; anger leads to hate; hate leads to suffering. - Yoda",
-    "It's a trap! - Admiral Ackbar"
+    "Que la Force soit avec toi. - Obi-Wan Kenobi",
+    "La Force sera toujours avec toi. - Obi-Wan Kenobi",
+    "Je trouve votre manque de foi troublant. - Dark Vador",
+    "Fais-le ou ne le fais pas. Il n'y a pas d'essai. - Yoda",
+    "À mon expérience, il n'y a pas de chance. - Obi-Wan Kenobi",
+    "La peur mène au côté obscur. La peur mène à la colère; la colère mène à la haine; la haine mène à la souffrance. - Yoda",
+    "C'est un piège! - Amiral Ackbar"
   ];
 
+  // Initialisation du plugin de notifications locales
   static void initialize() {
     _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
+    // Paramètres d'initialisation spécifiques à iOS
     const IOSInitializationSettings initializationSettingsIOS =
         IOSInitializationSettings(
       requestAlertPermission: true,
@@ -30,12 +33,15 @@ class NotificationManager {
       iOS: initializationSettingsIOS,
     );
 
+    // Initialisation du plugin avec les paramètres
     _flutterLocalNotificationsPlugin!.initialize(initializationSettings);
   }
 
+  // Démarrage des notifications périodiques
   static void startPeriodicNotifications() {
-    print('Starting periodic notifications...');
+    print('Démarrage des notifications périodiques...');
 
+    // Détails spécifiques à iOS pour les notifications
     const IOSNotificationDetails iosPlatformChannelSpecifics =
         IOSNotificationDetails(
       presentAlert: true,
@@ -47,26 +53,28 @@ class NotificationManager {
       iOS: iosPlatformChannelSpecifics,
     );
 
-    // Envoyer une notification toutes les 30 secondes
+    // Envoi d'une notification toutes les 5 secondes
     Timer.periodic(const Duration(seconds: 5), (timer) {
-      print('Sending periodic notification...');
-      // Choisissez une citation aléatoire de Star Wars
+      print('Envoi d\'une notification périodique...');
+      // Sélection d'une citation aléatoire de Star Wars
       String randomQuote = _getRandomStarWarsQuote();
 
+      // Affichage de la notification
       _flutterLocalNotificationsPlugin!.show(
         0,
-        'Star Wars Quote',
+        'Citation Star Wars',
         randomQuote,
         platformChannelSpecifics,
       );
     });
   }
 
+  // Fonction privée pour obtenir une citation aléatoire de Star Wars
   static String _getRandomStarWarsQuote() {
     Random random = Random();
     int index = random.nextInt(starWarsQuotes.length);
     String quote = starWarsQuotes[index];
-    print('Selected quote: $quote');
+    print('Citation sélectionnée : $quote');
     return quote;
   }
 }
